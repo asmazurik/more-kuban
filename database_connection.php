64e8de73f50f71e4e3bfc0844f07c357e6e64f20
Alexander Mazurik <a.s.mazurik@yandex.ru>
2014-05-06 12:37:54 +0400

    First code in repo

---
+++ database_connection.php
@@ -0,0 +1,4 @@
+<?
+mysql_connect("localhost", "wm32891_user", "123456");
+mysql_select_db("wm32891_menagerie");
+?>
---
+++ index.php
@@ -0,0 +1,31 @@
+<? //include("bd.php"); ?>
+
+<!doctype html>
+<html>
+<head>
+<meta charset="windows-1251">
+<title>Документ без названия</title>
+</head>
+
+<body>
+
+
+<form method="post">
+Ваше ФИО: <input name="fio"><br>
+Ваш телефон: <input name="phone"><br>
+Ваш email: <input name="phone"><br>
+<input type="submit" name="submit" value="Отправить заявку">
+</form>
+
+
+<?
+if(isset($_POST['submit'])) {
+	
+		mail("feomatar@list.ru","Заявка","Имя: ".$_POST['fio']."<br>Телефон: ".$_POST['phone']."<br>Email: ".$_POST['email'],"From: ".$_POST['name']." <robot@robot.ru>\r\ncontent-type:text/html; charset=windows-1251\r\n");
+		
+}
+?>
+
+
+</body>
+</html>
